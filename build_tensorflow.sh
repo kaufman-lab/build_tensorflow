301e8519b2242471ed4a6e296d871f8e5c30d760
Michael Young <myoung3@uw.edu>
2021-08-08 13:57:55 -0700

    fixup

--- build_tensorflow.sh
+++ build_tensorflow.sh
@@ -21,9 +21,8 @@
 #if[$march -ne 'cascadelake'] ||  ([$mtune == 'cascadelake'] && [march == 'cascadelake']); then
 
 #download tensorflow source
-git clone https://github.com/tensorflow/tensorflow
-mv ./tensorflow ./tensorflow$march$mtune
-git checkout r$tensorflowversion
+git clone https://github.com/tensorflow/tensorflow tensorflow$march$mtune
+cd tensorflow$march$mtune
 
 bazel build --config=mkl --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt="-march=${march}" --copt="-mtune=${mtune}" --copt="-O3" //tensorflow/tools/pip_package:build_pip_package
 
