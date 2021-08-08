3a83a8540e3ee4ab0c446eddc4f823b648259812
Michael Young <myoung3@uw.edu>
2021-08-08 13:52:08 -0700

    fixup

--- build_tensorflow.sh
+++ build_tensorflow.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 ## run this in the singularity container. 
+ #chmod u+x build_tensorflow.sh
+ #screen -S build
+ #export tensorflowversion=2.5
+ #export march='cascadelake'
+ #export mtune='cascadelake'
+ #singularity exec oras://ghcr.io/kaufman-lab/tensorflow_buildenvironment:v1 ./build_tensorflow.sh
  #note the singularity shell will try to inherit your current wd
  #singularity containers aren't writable
  #singularity containers mount your home directory by default
 
 ## the following variables need to be defined (note that singularity containers inherit exported variables)
- #export tensorflow-version=
+ #export tensorflowversion=
  #export march=
  #export mtune=
  
@@ -16,8 +22,8 @@
 
 #download tensorflow source
 git clone https://github.com/tensorflow/tensorflow
-mv tensorflow tensorflow$march$mtune
-git checkout r$tensorflow-version
+mv ./tensorflow ./tensorflow$march$mtune
+git checkout r$tensorflowversion
 
 bazel build --config=mkl --cxxopt="-D_GLIBCXX_USE_CXX11_ABI=0" --copt="-march=${{ march }}" --copt="-mtune=${{ mtune }}" --copt="-O3" //tensorflow/tools/pip_package:build_pip_package
 
